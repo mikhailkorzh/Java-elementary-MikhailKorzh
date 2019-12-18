9848e49a9f3eec00527bf570c1efa5ddbb4b5e1c
mikor <mikhail.korzh@gmail.com>
2019-12-18 13:44:28 +0200

    lecture 10 remastered

--- src/main/java/com/company/cource/homeworks/lecture10_builder/UrlBuilderApp.java
+++ src/main/java/com/company/cource/homeworks/lecture10_builder/UrlBuilderApp.java
@@ -2,15 +2,12 @@ package homeworks.lecture10_builder;
 
 public class UrlBuilderApp {
     public static void main(String[] args) {
-        UrlStructure.Builder urlStructure = new UrlStructure.Builder()
-                .withschemeSecure("HTTPS")
-                .withSubdomain("blog")
-                .withSecondLevelDomain("hubspot")
-                .withTopLevelDomain("com")
-                .withSubdirectory("marketing")
-                .withQueryString("?example=test&example2=test2")
-                .withHash("#section3");
-        System.out.println(urlStructure.build());
+        String urlStructure = new UrlStructure.Builder()
+                .withDomain("sitechecker.pro")
+                .withPath("/knowladge-base")
+                .build();
+
+        System.out.println(urlStructure);
 
     }
 }
--- src/main/java/com/company/cource/homeworks/lecture10_builder/UrlStructure.java
+++ src/main/java/com/company/cource/homeworks/lecture10_builder/UrlStructure.java
@@ -1,19 +1,30 @@
 package homeworks.lecture10_builder;
 
 public class UrlStructure {
-    String schemeNotSecure; //The scheme tells web servers which protocol to use when it accesses a page on your website.
-    String schemeSecure;
-    String subdomain; //A subdomain in a URL indicates which particular page of your website the web browser should serve up.
-    // For instance, subdomains like “blog” or “offers” will provide your website’s blog page or offers page.
-    String secondLevelDomain; //Your second-level domain (SLD) is the name of your website.
-    String topLevelDomain; //The top-level domain (TLD) specifies what type of entity your organization registers as on the internet.
-    String subdirectory; //A subdirectory helps people understand which particular section of a webpage they’re on.
-    String queryString; //contains data that is being passed to the page
-    String hash; //relates to a section within the page
+    private String protocol = "";
+    private String domain = "";
+    private String port = "";
+    private String path = "";
+    private String params = "";
 
-    @Override
-    public String toString() {
-        return "UrlStructure{" + schemeSecure + "//" + subdomain + "." + secondLevelDomain + "." + topLevelDomain + "/" + queryString + hash;
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getParams() {
+        return params;
     }
 
     public static class Builder {
@@ -23,43 +34,40 @@ public class UrlStructure {
             urlStructure = new UrlStructure();
         }
 
-        public Builder withschemeSecure(String schemeSecure) {
-            urlStructure.schemeSecure = schemeSecure;
+        public Builder withProtokol(String protokol) {
+            urlStructure.protocol = protokol.isEmpty() ? "" : protokol + "://";
             return this;
         }
 
-        public Builder withSubdomain(String subdomain) {
-            urlStructure.subdomain = subdomain;
+        public Builder withDomain(String domain) {
+            urlStructure.domain = domain;
             return this;
         }
 
-        public Builder withSecondLevelDomain(String secondLevelDomain) {
-            urlStructure.secondLevelDomain = secondLevelDomain;
+        public Builder withPort(Integer port) {
+            urlStructure.port = port.toString();
             return this;
         }
 
-        public Builder withTopLevelDomain(String topLevelDomain) {
-            urlStructure.topLevelDomain = topLevelDomain;
+        public Builder withPath(String path) {
+            urlStructure.path = path.startsWith("/") ? path : "/" + path;
             return this;
         }
 
-        public Builder withSubdirectory(String subdirectory) {
-            urlStructure.subdirectory = subdirectory;
+        public Builder withParam(String param) {
+            urlStructure.params += urlStructure.params.startsWith("?") ? param + "&" : "?" + param + "&";
             return this;
         }
 
-        public Builder withQueryString(String queryString) {
-            urlStructure.queryString = queryString;
+        public Builder withParam(String key, String value) {
+            String param = key + "=" + value + "&";
+            urlStructure.params += urlStructure.params.startsWith("?") ? param : "?" + param;
             return this;
         }
 
-        public Builder withHash(String hash) {
-            urlStructure.hash = hash;
-            return this;
-        }
-
-        public UrlStructure build() {
-            return urlStructure;
+        public String build() {
+            urlStructure.port = urlStructure.port.isEmpty() ? "" : ":" + urlStructure.port;
+            return urlStructure.protocol + urlStructure.domain + urlStructure.port + urlStructure.path + "?" + urlStructure.params;
         }
 
     }
