0fa580617da46d488620d15ed5c158456b95b4e7
mikor <mikhail.korzh@gmail.com>
2019-12-23 22:08:11 +0200

    homeWork about enums is added

---
+++ src/main/java/com/company/cource/homeworks/lecture11_enums/HoursBeforeEndOfWeek.java
@@ -0,0 +1,35 @@
+package homeworks.lecture11_enums;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HoursBeforeEndOfWeek {
+    public String getF() {
+        return null;
+    }
+
+    //String f = new SimpleDateFormat("EEEE").format(new Date()).toUpperCase();
+    Day day = Day.valueOf(new SimpleDateFormat("EEEE").format(new Date()).toUpperCase());
+    int sum = 0;
+    private int dayOfWeek;
+
+    public HoursBeforeEndOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String calculate() {
+        for (Day d : Day.values()) {
+            if (this.day.equals(Day.SATURDAY)) {
+                return Day.SATURDAY + " is a day off";
+            }
+            if (this.day.equals(Day.SUNDAY)){
+                return Day.SUNDAY + " is a day off";
+            }
+            if (d.getDaysSequenceNumber() >= this.dayOfWeek){
+                sum += d.getWorkingHoursQuantityPerDay();
+            }
+        }
+        return String.valueOf(sum);
+    }
+
+}
