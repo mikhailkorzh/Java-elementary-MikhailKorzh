bdea57b804198532f68ce97de5264cf3117882ec
mikor <mikhail.korzh@gmail.com>
2019-12-27 16:00:21 +0200

    lecture 10 remastered

---
+++ src/main/java/com/company/cource/homeworks/lecture10_builder/UrlBuilderApp.java
@@ -0,0 +1,15 @@
+package homeworks.lecture10_builder;
+
+public class UrlBuilderApp {
+    public static void main(String[] args) {
+        String urlStructure = new UrlStructure.Builder()
+                .withProtokol("http")
+                .withDomain("sitechecker.pro")
+                .withPath("/knowladge-base")
+                .withParam("page","1")
+                .build();
+
+        System.out.println(urlStructure);
+
+    }
+}
