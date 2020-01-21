6704cb223d3a3753e49bb616da9d3267bf49ca8f
mikor <mikhail.korzh@gmail.com>
2020-01-21 14:37:18 +0200

    New homeTask Collections added

---
+++ src/main/java/com/company/cource/homeworks/lecture16_collections/JukeboxApp.java
@@ -0,0 +1,9 @@
+package homeworks.lecture16_collections;
+
+import homeworks.lecture16_collections.start.Jukebox;
+
+public class JukeboxApp {
+    public static void main(String[] args) throws Exception {
+        new Jukebox().startUpload();
+    }
+}
